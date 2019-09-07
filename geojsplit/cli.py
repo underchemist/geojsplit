aea8479e7bb8ebf053f2bf7b74a0b6c75dd005c7
Yann-Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2019-09-07 12:48:36 -0700

    split cli and library code

---
+++ geojsplit/cli.py
@@ -0,0 +1,169 @@
+import argparse
+import json
+import logging
+import sys
+from logging.config import dictConfig
+from pathlib import Path
+from typing import List
+
+import geojson
+
+from .geojsplit import GeoJSONStreamer
+
+
+def input_geojson(args):
+    def gen_filename(filename: Path, file_count, width=None, parent=None):
+        if width is None:
+            width = 4
+        if parent is None:
+            parent = filename.parent
+        elif isinstance(parent, str):
+            parent = Path(parent)
+        suffix = filename.suffix
+        stem = filename.stem
+
+        return parent / (stem + "_x" + pad(file_count, width) + suffix)
+
+    logger = logging.getLogger(__name__)
+    logger.debug(f"starting splitting with geojson {args.geojson}")
+    gj = GeoJSONStreamer(args.geojson)
+    if args.limit > 0:
+        for count, features in enumerate(
+            gj.stream(
+                batch=args.geometry_count,
+                **{k: v for k, v in vars(args).items() if k != "geojson"},
+            )
+        ):
+            new_filename = gen_filename(
+                gj.geojson, count, width=args.suffix_length, parent=args.output
+            )
+            try:
+                if not args.dry_run:
+                    if not new_filename.parent.exists():
+                        logger.debug(f"creating output directory {args.output}")
+                        new_filename.parent.mkdir(parents=True, exist_ok=True)
+                    with new_filename.open("w") as fp:
+                        geojson.dump(features, fp)
+                logger.debug(
+                    f"successfully saved {len(features['features'])} features to {new_filename}"
+                )
+            except IOError as e:
+                logger.error(f"Could not write features to {new_filename}", exc_info=e)
+
+            if count >= args.limit - 1:
+                break
+
+
+def pad(file_count: int, width: int) -> str:
+    """
+    Generate sortable alphabetic string to append to filenames.
+
+    Arguments:
+        file_count (int): The current file count.
+        width (int): 
+    """
+    logger: logging.Logger = logging.getLogger(__name__)
+    alphabet: str = "abcdefghijklmopqrstuvwxyz"
+
+    if file_count >= len(alphabet) ** width:
+        logger.error(
+            f"Suffix of width of {width} is not enough to generate a unique filename. Increase "
+        )
+        sys.exit(1)
+
+    char_array: List[str] = []
+    for digit in range(width - 1, -1, -1):
+        digit_cap: int = len(alphabet) ** digit
+        idx: int = file_count // digit_cap
+        file_count -= idx * digit_cap
+        char_array.append(alphabet[idx])
+
+    return "".join(char_array)
+
+
+def setup_logger():
+    dct = {
+        "version": 1,
+        "formatters": {
+            "formatter": {
+                "class": "logging.Formatter",
+                "format": "%(name)s:%(levelname)s:%(message)s",
+            }
+        },
+        "handlers": {
+            "stream_handler": {
+                "class": "logging.StreamHandler",
+                "level": "DEBUG",
+                "formatter": "formatter",
+            }
+        },
+        "root": {"level": "ERROR", "handlers": ["stream_handler"]},
+    }
+    dictConfig(dct)
+
+
+def setup_parser():
+    parser = argparse.ArgumentParser(
+        prog="geojsplit", description="Split a geojson file into many geojson files."
+    )
+    parser.add_argument("geojson", help="filename of geojson file to split")
+    parser.add_argument(
+        "-l",
+        "--geometry-count",
+        type=int,
+        help="the number of features to be distributed to each file.",
+    )
+    parser.add_argument(
+        "-m",
+        "--max-lines",
+        type=int,
+        help="the number of lines to search through looking for a geojson features object",
+    )
+    parser.add_argument(
+        "-a",
+        "--suffix-length",
+        type=int,
+        help="number of characters in the suffix length for split geojsons",
+    )
+    parser.add_argument(
+        "-o", "--output", help="output directory to save split geojsons"
+    )
+    parser.add_argument(
+        "-n",
+        "--limit",
+        type=int,
+        help="limit number of split geojson files to at most n",
+    )
+    parser.add_argument(
+        "-v", "--verbose", help="increase output verbosity", action="store_true"
+    )
+    parser.add_argument(
+        "-d",
+        "--dry-run",
+        help="see output without actually writing to file",
+        action="store_true",
+    )
+
+    return parser
+
+
+def main():
+    setup_logger()
+    logger = logging.getLogger(__name__)
+    parser = setup_parser()
+    args = parser.parse_args()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    logger.debug(f"called {__name__} with arguments:")
+    for arg_name, arg_value in vars(args).items():
+        if arg_value is not None:
+            logger.debug(f"{arg_name}: {arg_value}")
+
+    input_geojson(args)
+    logger.debug(f"finished splitting geojson")
+
+
+if __name__ == "__main__":
+    main()
