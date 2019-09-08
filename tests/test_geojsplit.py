130a1511fd75e3baff997ffccb938ccbb7f84ced
Yann-Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2019-09-07 19:06:19 -0700

    add tests

--- tests/test_geojsplit.py
+++ tests/test_geojsplit.py
@@ -1,4 +1,11 @@
 import pytest
 
+from geojsplit import geojsplit
+
 __version__ = "0.1.0"
 
+
+def test_geojson_does_not_exist(tmp_path):
+    d = tmp_path / "fake.geojson"
+    with pytest.raises(FileNotFoundError):
+        geojsplit.GeoJSONBatchStreamer(d)
