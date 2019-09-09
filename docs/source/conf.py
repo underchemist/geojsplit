edf0b1cc0b5bef0ecf42944d5c2dd5389d4f5d5c
Yann-Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2019-09-08 19:02:36 -0700

    update config to make readthedocs use index as master_doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,9 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Config master doc for readthedocs
+master_doc = "index"
+
 
 # -- Options for HTML output -------------------------------------------------
 
